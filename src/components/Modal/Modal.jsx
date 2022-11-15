e1642cf68158d2fca663b586b0de6e2429768419
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-11-15 23:38:22 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,62 +1,62 @@
-// import { createPortal } from 'react-dom';
+import { createPortal } from 'react-dom';
 
-// import { useEffect } from 'react';
+import { useEffect } from 'react';
 
-// import s from './Modal.module.css';
-// import CloseIcon from '@mui/icons-material/Close';
-// import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-// import { useWindowWidth } from '@react-hook/window-size';
+import s from './Modal.module.css';
+import CloseIcon from '@mui/icons-material/Close';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useWindowWidth } from '@react-hook/window-size';
 
-// const modalRoot = document.querySelector('#modal-root');
+const modalRoot = document.querySelector('#modal-root');
 
-// const Modal = ({ onClose, children }) => {
-//     useEffect(() => {
-//         const handleKeyDown = e => {
-//             if (e.code === 'Escape') {
-//                 onClose(e);
-//             }
-//             document.body.style.overflow = 'hidden';
-//             return () => (document.body.style.overflow = 'unset');
-//         };
+const Modal = ({ onClose, children }) => {
+    useEffect(() => {
+        const handleKeyDown = e => {
+            if (e.code === 'Escape') {
+                onClose(e);
+            }
+            document.body.style.overflow = 'hidden';
+            return () => (document.body.style.overflow = 'unset');
+        };
 
-//         window.addEventListener('keydown', handleKeyDown);
-//         return () => window.removeEventListener('keydown', handleKeyDown);
-//     }, [onClose]);
+        window.addEventListener('keydown', handleKeyDown);
+        return () => window.removeEventListener('keydown', handleKeyDown);
+    }, [onClose]);
 
-//     const handleBackdropclick = e => {
-//         if (e.target === e.currentTarget) {
-//             onClose();
-//         }
-//     };
-//     const onlyWidth = useWindowWidth();
+    const handleBackdropclick = e => {
+        if (e.target === e.currentTarget) {
+            onClose();
+        }
+    };
+    const onlyWidth = useWindowWidth();
 
-//     return createPortal(
-//         <div className={s.overlay} onClick={handleBackdropclick}>
-//             <div className={s.modal}>
-//                 {onlyWidth <= 768 ? (
-//                     <div className={s.iconArrowBack}>
-//                         {' '}
-//                         <ArrowBackIcon onClick={onClose} />
-//                     </div>
-//                 ) : (
-//                     <CloseIcon
-//                         className={s.iconClose}
-//                         onClick={onClose}
-//                         sx={{
-//                             cursor: 'pointer',
-//                             margin: '-10px 0 10px 0',
-//                             marginLeft: 'auto',
-//                             textDecoration: 'none',
-//                             color: '#000000',
-//                             display: 'block',
-//                         }}
-//                     />
-//                 )}
-//                 <div className={s.wrapper}>{children}</div>
-//             </div>
-//         </div>,
-//         modalRoot
-//     );
-// };
+    return createPortal(
+        <div className={s.overlay} onClick={handleBackdropclick}>
+            <div className={s.modal}>
+                {onlyWidth <= 768 ? (
+                    <div className={s.iconArrowBack}>
+                        {' '}
+                        <ArrowBackIcon onClick={onClose} />
+                    </div>
+                ) : (
+                    <CloseIcon
+                        className={s.iconClose}
+                        onClick={onClose}
+                        sx={{
+                            cursor: 'pointer',
+                            margin: '-10px 0 10px 0',
+                            marginLeft: 'auto',
+                            textDecoration: 'none',
+                            color: '#000000',
+                            display: 'block',
+                        }}
+                    />
+                )}
+                <div className={s.wrapper}>{children}</div>
+            </div>
+        </div>,
+        modalRoot
+    );
+};
 
-// export default Modal;
+export default Modal;
